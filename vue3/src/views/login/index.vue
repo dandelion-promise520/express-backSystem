a8c780ed279cedb8f21f6cdbc44525ce88a6ed5a
dandelion <1993466159@qq.com>
2025-08-11 19:01:43 +0800

    登录界面配置tailwindcss与登录改为表单提交

--- vue3/src/views/login/index.vue
+++ vue3/src/views/login/index.vue
@@ -1,67 +1,98 @@
 <template>
-  <div class="common-layout">
+  <div>
     <el-container>
-      <el-header class="header-wrapped">
-        <div class="header-content">
-          <h3>通用后台管理系统</h3>
-          <span>欢迎登录</span>
-        </div>
+      <!-- 头部这块 -->
+      <el-header class="flex w-full justify-between items-center px-48!">
+        <h3>通用后台管理系统</h3>
+        <span class="text-sm">欢迎登录</span>
       </el-header>
-      <el-main class="main-wrapped">
-        <div class="main-content">
-          <el-card class="box-card">
-            <el-tabs v-model="activeName" class="demo-tabs" :stretch="true">
-              <el-tab-pane label="登录" name="first" label-position="right">
-                <el-form :model="loginForm" label-width="auto" >
-                  <el-form-item label="账号" >
+      <!-- 身体这块 -->
+      <el-main class="h-[750px] bg-[url(@/assets/img/loginBackImg.jpg)]">
+        <div class="w-full pr-42 h-full flex items-center justify-end">
+          <el-card class="h-96 w-sm">
+            <el-tabs v-model="activeName" :stretch="true">
+              <!-- 登录这块 -->
+              <el-tab-pane label="登录" name="first">
+                <!-- 表单这块 -->
+                <el-form
+                  size="large"
+                  class="mt-8"
+                  :hide-required-asterisk="true"
+                  :model="loginForm"
+                  ref="loginFormRef"
+                  label-width="auto"
+                  :rules="loginRules"
+                  @submit.native.prevent="Login"
+                >
+                  <el-form-item prop="account">
                     <el-input
                       v-model="loginForm.account"
-                      placeholder="请输入账号"
+                      name="account"
+                      placeholder="用户名"
+                      autocomplete="account"
                       clearable
                     />
                   </el-form-item>
-                  <el-form-item label="密码">
+                  <el-form-item prop="password">
                     <el-input
                       v-model="loginForm.password"
-                      placeholder="请输入密码"
-                      show-password
+                      name="password"
+                      autocomplete="password"
+                      placeholder="密码"
                       type="password"
+                      show-password
                       clearable
                     />
                   </el-form-item>
-                  <el-form-item label-width="auto" class="mt-4">
-                    <el-button type="primary" @click="Login" class="w-full h-12!">登录</el-button>
+                  <el-form-item label-width="auto">
+                    <el-button
+                      type="primary"
+                      native-type="submit"
+                      :loading="isLogging"
+                      class="w-full h-12! mt-4"
+                    >
+                      {{ isLogging ? "登录中..." : "登录" }}
+                    </el-button>
                   </el-form-item>
                 </el-form>
-                <div class="footer-wrapped">
-                  <div class="forget-password">
-                    <span class="forget-password-btn" @click="openForgetDialog"
+                <!-- 底部这块 -->
+                <div>
+                  <div class="flex justify-end">
+                    <span
+                      class="cursor-pointer text-primary-500 text-xs"
+                      @click="openForgetDialog"
                       >忘记密码</span
                     >
                   </div>
-                  <div class="go-register">
+                  <div class="flex justify-center text-sm">
                     还没有账号?<span
-                      class="go-register-btn"
+                      class="cursor-pointer text-primary-500"
                       @click="activeName = 'second'"
                       >去注册</span
                     >
                   </div>
                 </div>
               </el-tab-pane>
+
+              <!-- 注册这块  -->
               <el-tab-pane label="注册" name="second">
                 <el-form
-                  :model="registerForm"
+                  size="large"
                   label-width="auto"
-                  label-position="right"
+                  :hide-required-asterisk="true"
+                  ref="registerFormRef"
+                  :model="registerForm"
+                  :rules="registerRules"
+                  @submit.native.prevent="Register"
                 >
-                  <el-form-item label="账号">
+                  <el-form-item label="账号" prop="account" class="mt-4!">
                     <el-input
                       v-model="registerForm.account"
                       placeholder="账号长度为4~12位"
                       clearable
                     />
                   </el-form-item>
-                  <el-form-item label="密码">
+                  <el-form-item label="密码" prop="password" class="mt-8!">
                     <el-input
                       v-model="registerForm.password"
                       placeholder="密码长度为8~16位"
@@ -70,7 +101,11 @@
                       clearable
                     />
                   </el-form-item>
-                  <el-form-item label="确认密码">
+                  <el-form-item
+                    label="确认密码"
+                    prop="rePassword"
+                    class="mt-8!"
+                  >
                     <el-input
                       v-model="registerForm.rePassword"
                       placeholder="请再次输入密码"
@@ -80,7 +115,13 @@
                     />
                   </el-form-item>
                   <el-form-item>
-                    <el-button type="primary" @click="Register" class="w-full h-12!">注册</el-button>
+                    <el-button
+                      type="primary"
+                      @click="Register"
+                      class="w-full h-12! mt-4"
+                      :loading="isRegistering"
+                      >{{ isRegistering ? "注册中..." : "注册" }}</el-button
+                    >
                   </el-form-item>
                 </el-form>
               </el-tab-pane>
@@ -88,13 +129,12 @@
           </el-card>
         </div>
       </el-main>
-      <el-footer class="footer-wrapped">
-        <div class="footer-content">
-          <div class="title">
-            <span>小刘爱小朱</span> | <span>小朱爱小刘</span> |
-            <span>健健爱西西</span> |
-            <span>蔓蔓爱锋锋</span>
-          </div>
+      <!-- 玉足这块 -->
+      <el-footer class="w-full flex justify-center mt-4">
+        <div class="[&>span]:text-xs">
+          <span>小刘爱小朱</span> | <span>小朱爱小刘</span> |
+          <span>健健爱西西</span> |
+          <span>蔓蔓爱锋锋</span>
         </div>
       </el-footer>
     </el-container>
@@ -103,36 +143,79 @@
   </div>
 </template>
 
-<script setup lang="ts" name="Login">
+<script setup lang="ts">
 import { reactive, ref } from "vue";
 import forget_password from "./components/forget_password.vue";
 import { register, login } from "@/api/login";
-import { ElMessage } from "element-plus";
+import { ElMessage, type FormInstance, type FormRules } from "element-plus";
 import { passwordReg } from "@/utils/limit/index";
 import { useRouter } from "vue-router";
 const router = useRouter();
 import { useUserInfo } from "@/store/userInfo";
+import { rules } from "../userManagement/hooks/useRules";
 const { GetUserInfo } = useUserInfo();
 
-// 默认进来是登录
+// 默认进来是登录tab
 const activeName = ref("first");
-// 登录表单
-const loginForm: InterLogin = reactive({
-  account: "",
-  password: "",
-});
-// 注册表单
-const registerForm: InterLogin = reactive({
-  account: "",
-  password: "",
-  rePassword: "",
-});
-// 定义ref的标签
+
+// 忘记密码这块
 const forgetPassword = ref();
 // 打开弹框
 const openForgetDialog = () => {
   forgetPassword.value.openDialog();
 };
+
+// 注册逻辑这块
+interface registerFormInter {
+  account: string;
+  password: string;
+  rePassword: string;
+}
+
+// 判断是否已经在注册中
+const isRegistering = ref(false);
+
+// 注册表单
+const registerForm = reactive<registerFormInter>({
+  account: "",
+  password: "",
+  rePassword: "",
+});
+
+const registerFormRef = ref<FormInstance>();
+const registerRules = reactive<FormRules<registerFormInter>>({
+  account: [
+    {
+      required: true,
+      message: "您的账号不能为空",
+      trigger: "blur",
+    },
+  ],
+  password: [
+    {
+      required: true,
+      message: "您的密码不能为空",
+      trigger: "blur",
+    },
+    {
+      pattern: passwordReg,
+      message: "您的密码必须在8-16位之间，至少有一位大小写字母且有一个特殊字符",
+      trigger: "blur",
+    },
+  ],
+  rePassword: [
+    {
+      required: true,
+      message: "您的密码不能为空",
+      trigger: "blur",
+    },
+    {
+      pattern: passwordReg,
+      message: "您的密码必须在8-16位之间，至少有一位大小写字母且有一个特殊字符",
+      trigger: "blur",
+    },
+  ],
+});
 
 // 调用注册接口
 const Register = async () => {
@@ -177,34 +260,73 @@ const Register = async () => {
     ElMessage.error(error.message);
   }
 };
+
+// 登录逻辑这块
+interface loginFormInter {
+  account: string;
+  password: string;
+}
+// 判断是否已经在登录中
+const isLogging = ref(false);
+// 登录表单
+const loginForm = reactive<loginFormInter>({
+  account: "",
+  password: "",
+});
+const loginFormRef = ref<FormInstance>();
+
+const loginRules = reactive<FormRules<loginFormInter>>({
+  account: [
+    {
+      required: true,
+      message: "您的账号不能为空",
+      trigger: "blur",
+    },
+  ],
+  password: [
+    {
+      required: true,
+      message: "您的密码不能为空",
+      trigger: "blur",
+    },
+    {
+      pattern: passwordReg,
+      message: "您的密码必须在8-16位之间，至少有一位大小写字母且有一个特殊字符",
+      trigger: "blur",
+    },
+  ],
+});
+
 // 调用登录接口
 const Login = async () => {
-  if (!loginForm.account || !loginForm.password) {
-    ElMessage.error("您的账号或密码不能为空");
-    return;
-  }
-  try {
-    const res = await login(loginForm);
-    if (res.data.status == 1) {
-      ElMessage.error(res.data.message || "登录失败");
-      return;
+  if (!loginFormRef.value) return;
+  await loginFormRef.value.validate(async (valid) => {
+    if (!valid) return;
+    isLogging.value = true;
+    try {
+      const res = await login(loginForm);
+      if (res.data.status == 1) {
+        ElMessage.error(res.data.message || "登录失败");
+        isLogging.value = false;
+        return;
+      }
+      // 返回登录成功的结果
+      // console.log(res);
+      const { id } = res.data.results;
+      ElMessage({
+        message: "登录成功",
+        type: "success",
+      });
+      GetUserInfo(id);
+      localStorage.setItem("token", res.data.token);
+      localStorage.setItem("id", id);
+      setTimeout(() => {
+        router.push("/menu");
+      }, 1500);
+    } catch (error: any) {
+      ElMessage.error(error.message || "登录失败");
     }
-    // 返回登录成功的结果
-    // console.log(res);
-    const { id } = res.data.results;
-    ElMessage({
-      message: "登录成功",
-      type: "success",
-    });
-    GetUserInfo(id);
-    localStorage.setItem("token", res.data.token);
-    localStorage.setItem("id", id);
-    setTimeout(() => {
-      router.push("/menu");
-    }, 1500);
-  } catch (error: any) {
-    ElMessage.error(error.message || "登录失败");
-  }
+  });
 };
 
 // setTimeout(() => {
@@ -212,122 +334,3 @@ const Login = async () => {
 //   loginForm.password = "@Ljf1314520";
 // }, 500);
 </script>
-
-<style lang="css" scoped>
-/* 头部外壳 */
-.header-wrapped {
-  /* 头部内容 */
-  .header-content {
-    width: 1200px;
-    display: flex;
-    margin: 0 auto;
-    justify-content: space-between;
-    align-items: center;
-
-    span {
-      font-size: 13px;
-    }
-  }
-}
-
-/* 主体外壳 */
-.main-wrapped {
-  background-image: url(@/assets/img/loginBackImg.jpg);
-  height: 750px;
-
-  --el-main-padding: 0;
-
-  /* 主体内容 */
-  .main-content {
-    width: 1200px;
-    height: 750px;
-    margin: 0 auto;
-
-    /* 卡片样式 */
-    .box-card {
-      width: 350px;
-      height: 375px;
-      float: right;
-      position: relative;
-      top: 130px;
-
-      .el-form {
-        margin-top: 30px;
-      }
-
-      /* 卡片底部外壳 */
-      .footer-wrapped {
-        display: flex;
-        flex-direction: column;
-
-        /* 忘记密码 */
-        .forget-password {
-          display: flex;
-          justify-content: flex-end;
-          margin: 8px 0;
-
-          .forget-password-btn {
-            color: #409eff;
-            font-size: 12px;
-            cursor: pointer;
-          }
-        }
-
-        /* 去注册 */
-        .go-register {
-          display: flex;
-          justify-content: center;
-          font-size: 14px;
-
-          .go-register-btn {
-            color: #409eff;
-            cursor: pointer;
-          }
-        }
-      }
-    }
-  }
-}
-
-/* 页脚外壳 */
-.footer-wrapped {
-  margin: 8px 0;
-
-  /* 页脚内容 */
-  .footer-content {
-    width: 1200px;
-    margin: 0 auto;
-    display: flex;
-    justify-content: center;
-
-    span {
-      color: #666;
-      font-size: 12px;
-    }
-  }
-}
-
-/* 样式穿透 */
-/* tags标签 */
-:deep(.el-tabs__item) {
-  color: #333;
-  font-size: 18px;
-}
-
-/* 输入框高度 */
-:deep(.el-input__inner) {
-  height: 40px;
-}
-
-/* 输入框标签字体高度 */
-:deep(.el-form-item__label) {
-  line-height: 40px;
-  height: 40px;
-}
-
-/* 登录按钮 */
-/* :deep(.el-button) {
-  width: 240px;
-  height: 45px;
-} */
-</style>
